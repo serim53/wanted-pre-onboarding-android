a010be688c3d77b3c86faef0f204102476880a0d
serim53 <tpfla9953@naver.com>
2024-03-01 15:07:43 +0900

    [FEAT] Add Coroutine Concept Code

---
+++ Coroutine/app/src/main/java/com/example/coroutine/CoroutineConcept.kt
@@ -0,0 +1,23 @@
+package com.example.coroutine
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun main() {
+    runBlocking {
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default) {
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found.")
+            }
+            println("${Thread.currentThread().name} - end of launch function")
+        }
+        println("Loading...")
+    }
+}
